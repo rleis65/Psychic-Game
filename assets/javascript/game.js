12cb5bdbce05f6b73029d219bf00ce764b197df8
Ryan Leis <rleis65@gmail.com>
2018-02-03 21:01:24 -0600

    working code

---
+++ assets/javascript/game.js
@@ -0,0 +1,52 @@
+var alphabetLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+
+var guessesSoFar = [];
+
+var userGuess = null;
+
+var letterToBeGuessed = alphabetLetters[Math.floor(Math.random() * alphabetLetters.length)];
+("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far: " + guessesSoFar + " Computer picked: " + letterToBeGuessed);
+
+
+document.onkeyup = function(event) {
+
+	
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+	
+	if (guessesSoFar.indexOf(userGuess) < 0 && alphabetLetters.indexOf(userGuess) >= 0) {
+		guessesSoFar[guessesSoFar.length]=userGuess;
+		
+		guessesLeft--;
+	}
+
+	if (letterToBeGuessed == userGuess) {
+		wins++;
+		alert("You won!");
+		guessesLeft = 9;
+		guessesSoFar = [];
+		letterToBeGuessed = alphabetLetters[Math.floor(Math.random() * alphabetLetters.length)];
+		
+	}
+
+	
+	if (guessesLeft == 0) {
+		losses++;
+		alert("You lost!");
+		guessesLeft = 9;
+		guessesSoFar = [];
+		letterToBeGuessed = alphabetLetters[Math.floor(Math.random() * alphabetLetters.length)];
+		
+	}
+
+	
+	var html = "<p><h1>The Psychic Game</h1></p>" + "<p><h4>Guess what letter I\'m thinking of</h4></p>" + "<p><h4>Wins: " + wins + "</h4></p>" + "<p><h4>Losses: " + losses + "</h4></p>" + "<p><h4>Guesses Left: " + guessesLeft + "</h4></p>" + "<p><h4>Your guesses so far: " + guessesSoFar + "</h4></p>";
+	
+	document.querySelector("#game").innerHTML = html;
+
+}
